5b175a7921d8408ba63a9a7f8b5038822a663bdd
0yah <highmeme10@gmail.com>
2021-02-12 22:04:46 +0300

    Initialize Login UI

--- src/App.js
+++ src/App.js
@@ -17,21 +17,24 @@ import { Update } from "./components/Update";
 const App=()=> {
   return (
 <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
 
+
+      <div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">House Finder</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="/login">Login</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,29 @@
-export const Login = ()=>{
+import React,{useEffect,useState} from 'react'
+import bsnbsd from '../img/loginbg.jpg';
 
-    return <div>
-        
+const bg = require('../img/loginbg.jpg');
+
+
+export const Login = () => {
+
+   return <div className="loginLayout">
+  
+        <div className="slides">
+            <img src="https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1934&q=80"/>
+
+            <div className="content">
+            <h1>Login</h1>
+        <form>
+            <input type="email" placeholder="Email Address"/>
+            <input type="password" placeholder="Password"/>
+            <button>Login</button>
+            <span>Create Account</span>
+        </form>
+        </div>
+        </div>
+
+
+
+
     </div>
 }
